80698bfe978de78d2660265d20ecfa10a6c0e798
Camilo Andres Hernandez Molina <camilo.hernandez.molina@gopenux.com>
2023-06-10 17:26:00 -0500

    Rest bundle instalado

--- config/bundles.php
+++ config/bundles.php
@@ -24,4 +24,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
