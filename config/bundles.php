9b27b6bfe15c9b70cb7c6675eb236370a59bdb87
Camilo Andres Hernandez Molina <camilo.hernandez.molina@gopenux.com>
2023-06-21 15:54:43 -0500

    UploaderSerializationBundle instalado

--- config/bundles.php
+++ config/bundles.php
@@ -31,4 +31,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Fresh\VichUploaderSerializationBundle\FreshVichUploaderSerializationBundle::class => ['all' => true],
 ];
